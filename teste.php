9097b4e105a3f4c4f55ac3c95bb5ccfd8768ba5c
clezer aragon <toboyramos@gmail.com>
2014-08-23 21:26:29 -0300

    testes php

---
+++ teste.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: klezer
+ * Date: 5/31/14
+ * Time: 2:19 AM
+ */
+
+echo 'clezer aragon ramos';
+
+echo 'bom dia';
+
+for ($i = 0; $i< 10; $i++){
+
+    var_dump($i);
+
+    echo $i.'  clezer'.'<br>';
+
+    print_r('isso e um print: '.$i);
+}
+
+
+
+
+
+
+
+
+
